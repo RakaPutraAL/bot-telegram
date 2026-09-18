5d57347345cf9ee5b721eab93bbd8fb9531c80d8
Raka Putra <aryaletaarya@gmail.com>
2026-09-18 21:59:16 +0700

    Add deployment config

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT", "28512")),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+}
